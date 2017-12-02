01e78c5f877bea5731dca50835998a8da2e70f22
Firmino Changani <flowck96@gmail.com>
2017-12-02 14:39:21 +0100

    Add proper error handling

--- app/middlewares/controloDeAcesso.js
+++ app/middlewares/controloDeAcesso.js
@@ -1,10 +1,25 @@
-module.exports = (req, res, next) => {
+// Controlo de acesso para os candidatos
+module.exports.Candidatos = (req, res, next) => {
 
 	// Utilizador autorizado com sessão iniciada
 	let utilizadorAutorizado = req.session.email;
 
 	if (!utilizadorAutorizado) {
-		res.redirect('/login');
+		res.redirect('/login/candidato');
+	} else {
+		next();
+	}
+
+}
+
+// Controlo de acesso para os Empregadores
+module.exports.Empregadores = (req, res, next) => {
+
+	// Utilizador autorizado com sessão iniciada
+	let utilizadorAutorizado = req.session.email;
+
+	if (!utilizadorAutorizado) {
+		res.redirect('/login/empregador');
 	} else {
 		next();
 	}
