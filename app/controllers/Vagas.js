ce58e3bdee43544c63830658f20c4022a9e21f3c
Firmino Changani <flowck96@gmail.com>
2017-12-03 14:42:50 +0100

    New controller to handle jobs positions

---
+++ app/controllers/Vagas.js
@@ -0,0 +1,9 @@
+// Dependências
+const db = require('../models/database');
+const EnviarMail = require('./EnviarMail');
+const moment = require('moment');
+
+/*
+*	ListarVagas: Esta função é responsável por listar as vagas com base num id
+*/
+//module.exports = 
