35a623107652a85ddf7fd046bcb395db05cc4454
Firmino Changani <flowck96@gmail.com>
2017-12-09 10:29:53 +0100

    Get job positions
    This module is responsible for querying the job position in the database

--- app/controllers/Vagas.js
+++ app/controllers/Vagas.js
@@ -3,7 +3,44 @@ const db = require('../models/database');
 const EnviarMail = require('./EnviarMail');
 const moment = require('moment');
 
+// Alterar idioma do módulo Momentjs
+moment.locale('pt');
+
 /*
 *	ListarVagas: Esta função é responsável por listar as vagas com base num id
 */
-//module.exports = 
+module.exports.listarVagas = (req, res) => {
+
+	let ID = req.session.ID;
+
+	let sql = `SELECT * FROM Vaga WHERE Empregador_idEmpregador = ?`;
+		sql = db.format(sql, ID);
+	db.query(sql, (err, resultado) => {
+		if(resultado.length > 0){
+
+			// Gerar um novo array com os dados da vaga
+			var Vagas = resultado.map((vaga) => {
+				/* Dias restantes a partir da data limite e da 
+					data de publicacao
+				 */ 
+				let dataDePublicacao = moment(vaga.data_de_publicacao);
+				let dataLimite = moment(vaga.data_limite);
+				let diasRestantes = dataLimite.from(dataDePublicacao, true);
+
+				return {
+					ID: vaga.idVaga,
+					cargo: vaga.cargo,
+					dataDePublicacao: moment(vaga.data_de_publicacao).format('DD-MM-YYYY'),
+					/*totalDeCandidaturas: vaga.total_de_candidaturas*/
+					diasRestantes: diasRestantes
+				}
+			});
+
+			res.render('empregador/empregador-home', {Vagas});
+
+		} else {
+			return "";
+		}
+	});
+
+}
