eadc1620068378c197eeb7c66f78970ced3bf6a8
Firmino Changani <flowck96@gmail.com>
2017-12-25 19:36:40 +0100

    Get a single job

--- app/controllers/Vagas.js
+++ app/controllers/Vagas.js
@@ -20,6 +20,7 @@ module.exports.listarVagas = (req, res) => {
 
 			// Gerar um novo array com os dados da vaga
 			var Vagas = resultado.map((vaga) => {
+				
 				/* Dias restantes a partir da data limite e da 
 					data de publicacao
 				 */ 
@@ -34,6 +35,7 @@ module.exports.listarVagas = (req, res) => {
 					estado: vaga.estado,
 					diasRestantes: diasRestantes
 				}
+
 			});
 
 			res.render('empregador/empregador-home', {Vagas});
@@ -45,6 +47,62 @@ module.exports.listarVagas = (req, res) => {
 
 }
 
+/*
+*	removerVaga: Esta função é responsável por obter uma vaga com base num id
+*/
+module.exports.obterVaga = (req, res) => {
+	// Obter o id a partir da URL. Ex: /vaga/3
+	// e converter o id obtido da URL para o tipo de dados
+	// number
+	let ID = Number(req.params.idVaga);
+
+	// Validação do id que é passado na URL, caso não for um número então é redirecionado para a home
+	if (typeof(ID) === 'number') {
+		// Query para obter uma vaga
+		let sql = `SELECT idVaga, cargo, descricao, data_de_publicacao, provincia, tipo_de_contrato,
+		anos_de_experiencia, Vaga.area_de_actuacao, habilidades_necessarias, salario, quantidade_de_vagas,
+		nome FROM Vaga, Empregador WHERE idVaga = ? AND Empregador_idEmpregador = idEmpregador`;
+
+		// Preparar a query
+		sql = db.format(sql, ID);
+
+		// Executar a query
+		db.query(sql, (err, vaga) => {
+
+			if(err) throw err;
+
+			vaga.ID = vaga.idVaga;
+			// Formatar a data
+		
+			vaga = vaga.map((v) => {
+				return {
+					ID: v.idVaga,
+					cargo: v.cargo,
+					nome: v.nome,
+					descricao: v.descricao,
+					dataDePublicacao: moment(v.data_de_publicacao).format('DD-MM-YYYY'),
+					provincia: v.provincia,
+					tipoDeContrato: v.tipo_de_contrato,
+					anosDeExperiencia: v.anos_de_experiencia,
+					areaDeActuacao: v.area_de_actuacao,
+					habilidadesNecessarias: v.habilidades_necessarias,
+					salario: v.salario.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'}),
+					quantidadeDeVagas: v.quantidade_de_vagas
+				}
+			});
+
+			console.log(vaga[0]);
+			res.render('vaga', {Vaga: vaga[0]});
+		});
+	} else {
+		res.redirect('/')
+	}
+}
+
+
+/*
+*	removerVaga: Esta função é responsável por remover a vaga com base num id
+*/
 module.exports.removerVaga = (req, res) => {
 
 	// Id da vaga
@@ -61,4 +119,43 @@ module.exports.removerVaga = (req, res) => {
 		res.redirect('/empregador');
 	});
 
+}
+
+/*
+*	viewEditarVaga: Esta função é responsável por renderizar a view editarVaga
+*/
+
+module.exports.viewEditarVaga = (req, res) => {
+
+	// ID da vaga
+	let ID = req.params.id;
+
+	// Query para obter a vaga por ID
+	let sql = `SELECT * FROM Vaga WHERE idVaga = ?`;
+		sql = db.format(sql, ID);
+
+	db.query(sql, (err, vaga) => {
+
+		if(err) throw err;
+
+		// Verifica se o ID existe na base de dados
+		if (vaga.length > 0) {
+
+			vaga[0].data_limite = moment(vaga[0].data_limite).format('YYYY-MM-DD');
+			res.render('empregador/editar-vaga', {Vaga: vaga[0]})
+
+		} else {
+			// Se o ID não constar na base de dados então retorna para a home do empregador
+			res.redirect('/empregador');
+		}
+
+	});
+
+}
+
+/*
+*	rditarVaga: Esta módulo é responsável por actualizar as informações da vaga na bd
+*/
+module.exports.editarVaga = (req, res) => {
+	
 }
